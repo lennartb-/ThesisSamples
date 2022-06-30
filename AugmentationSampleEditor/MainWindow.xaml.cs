e5493be4be64763c564c941f9ffb976bd99da1c4
Lennart Brüggemann <lbrueggemann@scopeland.de>
2022-06-30 15:02:18 +0200

    Add remaining demos

--- AugmentationSampleEditor/MainWindow.xaml.cs
+++ AugmentationSampleEditor/MainWindow.xaml.cs
@@ -1,16 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using AugmentationFramework.AdviceDisplay;
 using AugmentationFramework.Augmentations;
-using AugmentationFramework.Augmentations.Premade;
-using AugmentationFramework.Renderer.Premade;
 using AugmentationSampleEditor.ViewModels;
-using Microsoft.CodeAnalysis.Differencing;
 
 namespace AugmentationSampleEditor;
 
@@ -21,22 +14,15 @@ public partial class MainWindow : Window
 {
     private readonly IList<Augmentation> plugins = new List<Augmentation>();
 
-    public IList<ISampleContent> ViewModels { get; } = new List<ISampleContent>();
-
     public MainWindow()
     {
         InitializeComponent();
         ViewModels.Add(new SynonymDisplayVm());
         ViewModels.Add(new SmalltalkHighlightingVm());
         ViewModels.Add(new DecorationsVm());
+        ViewModels.Add(new HashingVm());
+        ViewModels.Add(new AdviceVm());
         DataContext = this;
-        
-        //var backgroundAugmentation = new Augmentation(Editor.TextArea)
-        //    .WithForeground(Brushes.RoyalBlue)
-        //    .WithFontWeight(FontWeights.Bold)
-        //    .WithFontFamily(new FontFamily("Consolas"))
-        //    .ForText(new Regex(@"\bsit\b"));
-        //plugins.Add(backgroundAugmentation);
 
         var image = new BitmapImage(
             new Uri("pack://application:,,,/Resources/ic_menu_save.png"));
@@ -46,34 +32,7 @@ public partial class MainWindow : Window
         //.WithImagePosition(ImagePosition.Left)
         //.ForText(new Regex(@"\btristique\b"));
         //plugins.Add(underlineAugmentation);
-
-        //var tooltipAugmentation = new Augmentation(Editor.TextArea)
-        //.WithTooltip(() => new Calendar())
-        //.WithOverlay(
-        //    () => new Button
-        //        { Content = "Hallo" })
-        //.WithBackground(Brushes.LightGreen)
-        //.ForText(new Regex(@"\bodio\b"));
-        //plugins.Add(tooltipAugmentation);
-
-        //foreach (var augmentation in SmalltalkHighlighting.GetAugmentation(Editor.TextArea.TextView))
-        //{
-        //    plugins.Add(augmentation);
-        //}
-
-        //var underlineAugmentation = new Augmentation(Editor.TextArea)
-        //    .WithDecorationColor(Brushes.Red)
-        //    .WithDecoration(UnderlineBracket.Geometry)
-        //    .WithTooltip("SHA1 is cryptographically broken, please use a currently secure function like SHA-512.")
-        //    .ForText(new Regex(@"\.HashWithSha1(.*)?\)"));
-
-        //var underlineAugmentation = new Augmentation(Editor.TextArea.TextView)
-        //    .WithDecorationColor(Brushes.Red)
-        //    .WithDecoration(UnderlineBracket.Geometry)
-        //    .WithAdviceOverlay(new SampleAdviceModel())
-        //    .ForText(new Regex(@"\.HashWithSha1(.*)?\)"));
-
-        //plugins.Add(underlineAugmentation);
-
     }
+
+    public IList<ISampleContent> ViewModels { get; } = new List<ISampleContent>();
 }
---
+++ AugmentationSampleEditor/ViewModels/AdviceVm.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Reactive;
+using System.Text.RegularExpressions;
+using System.Windows.Media;
+using AugmentationFramework.AdviceDisplay;
+using AugmentationFramework.Augmentations;
+using AugmentationFramework.Renderer.Premade;
+using ICSharpCode.AvalonEdit.Document;
+using ReactiveUI;
+using RoslynPad.Editor;
+
+namespace AugmentationSampleEditor.ViewModels;
+
+public class AdviceVm : ISampleContent
+{
+    private const string Text = @"var hashedString = HashingAlgorithms.HashWithSha1(""Lorem ipsum dolor sit amet"")";
+    private readonly IList<Augmentation> augmentations = new List<Augmentation>();
+    private bool isEnabled;
+
+    public AdviceVm()
+    {
+        var stringTextSource = new StringTextSource(Text);
+        Document = new TextDocument(stringTextSource);
+        EditorLoadedCommand = ReactiveCommand.Create<CodeTextEditor>(OnLoaded);
+    }
+
+    public ReactiveCommand<CodeTextEditor, Unit> EditorLoadedCommand { get; }
+    public TextDocument Document { get; }
+
+    public bool IsEnabled
+    {
+        get => isEnabled;
+        set
+        {
+            isEnabled = value;
+            if (isEnabled)
+            {
+                foreach (var augmentation in augmentations) augmentation.Enable();
+            }
+            else
+            {
+                foreach (var augmentation in augmentations) augmentation.Disable();
+            }
+        }
+    }
+
+    public string Title => "Security Advice";
+
+    private void OnLoaded(CodeTextEditor editor)
+    {
+        var underlineAugmentation = new Augmentation(editor.TextArea.TextView)
+            .WithDecorationColor(Brushes.Red)
+            .WithDecoration(UnderlineBracket.Geometry)
+            .WithAdviceOverlay(new SampleAdviceModel())
+            .ForText(new Regex(@"\.HashWithSha1(.*)?\)"));
+
+        augmentations.Add(underlineAugmentation);
+    }
+}
