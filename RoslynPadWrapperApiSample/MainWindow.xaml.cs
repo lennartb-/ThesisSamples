e348a50c495c6e421ed7cf98f3895f96c103157b
Lennart Brüggemann <lbrueggemann@scopeland.de>
2022-07-07 15:42:24 +0200

    Add wrapper API samples

---
+++ RoslynPadWrapperApiSample/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Reflection;
+using System.Windows;
+using RoslynPad.Editor;
+using RoslynPad.Roslyn;
+
+namespace RoslynPadWrapperApiSample
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnEditorLoaded(object sender, RoutedEventArgs e)
+        {
+            var editor = (RoslynCodeEditor)sender;
+            Loaded -= OnEditorLoaded;
+
+            var host = new RoslynHost(additionalAssemblies: new[]
+            {
+                Assembly.Load("RoslynPad.Roslyn.Windows"),
+                Assembly.Load("RoslynPad.Editor.Windows")
+            }, RoslynHostReferences.NamespaceDefault.With(assemblyReferences: new[]
+            {
+                typeof(object).Assembly,
+                typeof(WrapperApiSample.HashFunctions).Assembly,
+            }));
+
+            var documentId = editor.Initialize(host, new ClassificationHighlightColors(),
+                "C:\\WorkingDirectory", string.Empty);
+        }
+    }
+}
---
+++ WrapperApiSample/HashFunctions.cs
@@ -0,0 +1,56 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace WrapperApiSample
+{
+    public static class HashFunctions
+    {
+        /// <summary>
+        /// Hashes a string using the SHA512 algorithm.
+        /// </summary>
+        /// <param name="input">An UTF8 string to hash.</param>
+        /// <returns>The SHA512-hashed equivalent of <paramref name="input"/>,
+        /// as a uppercase hex string without dashes.</returns>
+        public static string HashAsSha512(this string input)
+        {
+            using (var sha512Hash = SHA512.Create())
+            {
+                var inputBytes = Encoding.UTF8.GetBytes(input);
+                var hashedBytes = sha512Hash.ComputeHash(inputBytes);
+                return Convert.ToHexString(hashedBytes);
+            }
+        }
+
+        /// <summary>
+        /// Hashes a string using the SHA256 algorithm.
+        /// </summary>
+        /// <param name="input">An UTF8 string to hash.</param>
+        /// <returns>The SHA256-hashed equivalent of <paramref name="input"/>,
+        /// as a uppercase hex string without dashes.</returns>
+        public static string HashAsSha256(this string input)
+        {
+            using (var sha256Hash = SHA256.Create())
+            {
+                var inputBytes = Encoding.UTF8.GetBytes(input);
+                var hashedBytes = sha256Hash.ComputeHash(inputBytes);
+                return Convert.ToHexString(hashedBytes);
+            }
+        }
+
+        /// <summary>
+        /// Hashes a string using the MD5 algorithm.
+        /// </summary>
+        /// <param name="input">An UTF8 string to hash.</param>
+        /// <returns>The MD5-hashed equivalent of <paramref name="input"/>,
+        /// as a uppercase hex string without dashes.</returns>
+        public static string HashAsMd5(this string input)
+        {
+            using (var md5Hash = MD5.Create())
+            {
+                var inputBytes = Encoding.UTF8.GetBytes(input);
+                var hashedBytes = md5Hash.ComputeHash(inputBytes);
+                return Convert.ToHexString(hashedBytes);
+            }
+        }
+    }
+}
