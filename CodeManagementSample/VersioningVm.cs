b3f7405b20df6b2af9a373e91b4f56e24757ae5b
Lennart Brüggemann <lbrueggemann@scopeland.de>
2022-09-29 15:08:58 +0200

    Add Versioning window

---
+++ CodeManagementSample/VersioningVm.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using ICSharpCode.AvalonEdit.Document;
+using LibGit2Sharp;
+
+namespace CodeManagementSample;
+internal class VersioningVm
+{
+    private string repositoryPath;
+
+    public RelayCommand OkCommand { get; }
+    public RelayCommand CancelCommand { get; }
+    public RelayCommand PushCommand { get; }
+    public RelayCommand PullCommand { get; }
+
+    public string EntityName { get; set; }
+    public TextDocument PreviewDocument { get; set; }
+
+    public IEnumerable<Commit> History { get; }
+
+    public VersioningVm(string repositoryPath)
+    {
+        this.repositoryPath = repositoryPath;
+    }
+
+    public void InitOrOpenRepository()
+    {
+        var existingRepo = Repository.Discover(repositoryPath);
+
+        if (string.IsNullOrEmpty(existingRepo))
+        {
+            Repository.Init(repositoryPath);
+        }
+    }
+
+    public void CommitCode()
+    {
+
+    }
+
+    public void PullCode()
+    {
+
+    }
+}
