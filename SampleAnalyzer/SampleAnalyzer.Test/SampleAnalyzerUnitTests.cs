1b7154241b96d96ff1d5cb98d85dd6ed2520587d
Lennart Brüggemann <lbrueggemann@scopeland.de>
2022-11-03 15:18:16 +0100

    Add SampleAnalyzer

---
+++ SampleAnalyzer/SampleAnalyzer.Test/SampleAnalyzerUnitTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VerifyCS = SampleAnalyzer.Test.CSharpCodeFixVerifier<
+    SampleAnalyzer.SampleAnalyzerAnalyzer,
+    SampleAnalyzer.SampleAnalyzerCodeFixProvider>;
+
+namespace SampleAnalyzer.Test
+{
+    [TestClass]
+    public class SampleAnalyzerUnitTest
+    {
+        //No diagnostics expected to show up
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            var test = @"";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+
+        //Diagnostic and CodeFix both triggered and checked for
+        [TestMethod]
+        public async Task TestMethod2()
+        {
+            var test = @"
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+
+    namespace ConsoleApplication1
+    {
+        class Testclass
+        {   
+            public string Test = ""new\nline"";
+        }
+    }";
+
+            await VerifyCS.VerifyAnalyzerAsync(test);
+        }
+    }
+}
---
+++ SampleAnalyzer/SampleAnalyzer/SampleAnalyzerAnalyzer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace SampleAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class SampleAnalyzerAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "SampleAnalyzer";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Naming";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxNodeAction(AnalyzeSymbol, SyntaxKind.StringLiteralExpression);
+        }
+
+        private static void AnalyzeSymbol(SyntaxNodeAnalysisContext context)
+        {
+            var literalNode = context.Node as LiteralExpressionSyntax;
+
+            // Find just those named type symbols with names containing lowercase letters.
+            if (literalNode?.Token.ValueText.Contains("\n") == true)
+            {
+                // For all such symbols, produce a diagnostic.
+                var diagnostic = Diagnostic.Create(Rule, literalNode.GetLocation(), literalNode.Token.ValueText);
+
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
