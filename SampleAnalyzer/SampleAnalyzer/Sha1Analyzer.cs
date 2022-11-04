ce49fd173988e24a952ddcf3215456a37afa8123
Lennart Brüggemann <lbrueggemann@scopeland.de>
2022-11-04 11:19:00 +0100

    Add Sha1Analyzer

---
+++ SampleAnalyzer/SampleAnalyzer/Sha1Analyzer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace SampleAnalyzer
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class Sha1Analyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "Sha1Analyzer";
+        private const string Category = "Security";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.Sha1AnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.Sha1AnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.Sha1AnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, true, Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxNodeAction(AnalyzeSymbol, SyntaxKind.StringLiteralExpression);
+        }
+
+        private static void AnalyzeSymbol(SyntaxNodeAnalysisContext context)
+        {
+            var literalNode = context.Node as LiteralExpressionSyntax;
+
+            // Find just those named type symbols with names containing lowercase letters.
+            if (literalNode?.Token.ValueText.Contains("\n") == true)
+            {
+                // For all such symbols, produce a diagnostic.
+                var diagnostic = Diagnostic.Create(Rule, literalNode.GetLocation(), literalNode.Token.ValueText);
+
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+    }
+}
